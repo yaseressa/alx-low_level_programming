ba0b11f6e34b7556913b0053730159fc00fe76a4
yaseressa <yaseressa222@gmail.com>
2022-10-13 13:03:07 -0700

    uihh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
-* main -is the entry point for the program.   
-* Return:zero if no error, Non-zero value  if errors.
+*main -is the entry point for the program.   
+*Return:zero if no error, Non-zero value  if errors.
 */
 int main(void)
 {
