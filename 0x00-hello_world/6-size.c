902894c0c743c3e118487bf063fe5d542b6ad31c
yaseressa <yaseressa222@gmail.com>
2022-10-13 13:27:35 -0700

    lol

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/**                                                                                                                                                    
-* main - Entry point                                                                                                                                
-* Return: Always 0 (Success)                                                                                                                        
-*/ 
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 printf("Size of a char: %ld byte(s)\n", sizeof(char));
