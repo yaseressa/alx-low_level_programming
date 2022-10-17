acc146b35bb6ef59afa1f92b023436dd001e4b86
yaseressa <yaseressa222@gmail.com>
2022-10-17 13:13:19 -0700

    uhfewui

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,22 +6,30 @@
  */
 int main(void)
 {
-int i, j;
+int i,  k, j;
 for (i = 48; i <= 57; i++)
 {
 for (j = 48; j <= 57; j++)
 {
 for (k = 48; k <= 57; k++)
 {
+if (i < j && j < k)
+{
 putchar(i);
 putchar(j);
 putchar(k);
-if (i != 57 && j != 57 && k != 57)
+if (i == 55 && j == 56 && k == 57)
+{
+break;
+}
+else
 {
 putchar(',');
+putchar(' ');
 }
 }
 }
-putchar('\n');
+}
+}
 return (0);
 }
