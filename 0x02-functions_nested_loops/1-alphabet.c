6b90adcc19034e564854d53201fdd2a3ed6234ad
yaseressa <yaseressa222@gmail.com>
2022-10-18 13:19:08 -0700

    jjiore

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *main-program entry point.
+ *Return:0 if no error, non-zero value if error.
+ */
+int main(void)
+{
+int i;
+for (i = 97; i <= 127; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+return (0);
+}
