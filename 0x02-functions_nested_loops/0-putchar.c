1763a302775de49340d3a5f3c8b65ec56749699a
yaseressa <yaseressa222@gmail.com>
2022-10-18 12:01:30 -0700

    uhefwu

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main-program entry point.
+ *Return:0 if no error, non-zero value if error.
+ */
+int main(void)
+{
+printf("_putchar");
+putchar('\n');
+}
