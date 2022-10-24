b752dc6ff4e421f6d7c01d5e4fd4049df636d2e2
yaseressa <yaseressa222@gmail.com>
2022-10-24 14:20:48 -0700

    eruihi

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
-
 /**
- *_strcpy - copies the string pointed to by src
+ * *_strcpy - copies the string pointed to by src
  * including the terminating null byte (\0)
  * to the buffer pointed to by dest
  * @dest: pointer to the buffer in which we copy the string
@@ -12,22 +11,20 @@
 
 char *_strcpy(char *dest, char *src)
 {
-  int len, i;
+	int len, i;
 
-  len = 0;
-  
-  while (src[len] != '\0')  
-  {    
-    len++;  
-  }
-  
-  for (i = 0; i < len; i++)  
-    {
-      dest[i] = src[i];  
-    }
-  
-  dest[i] = '\0';
-  
-  return (dest);
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len; i++)
+    	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+	return (dest);
   
 }
