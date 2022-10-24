f10b554867133e0414700e8ee6105c1db010b9eb
yaseressa <yaseressa222@gmail.com>
2022-10-24 14:35:25 -0700

    id

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,30 +1,27 @@
 #include "main.h"
 
 /**
- * *_strcpy - copies the string pointed to by src
- * including the terminating null byte (\0)
- * to the buffer pointed to by dest
- * @dest: pointer to the buffer in which we copy the string
- * @src: string to be copied
- * Return: the pointer to dest
+ *puts_half - prints every other character of a string
+ *@str: A pointer to an int that will be changed
+ *Return: void which means our answer is correct
  */
 
-char *_strcpy(char *dest, char *src)
+void puts_half(char *str)
 {
-	int len, i;
+	int i, last;
 
-	len = 0;
-
-	while (src[len] != '\0')
+	i = 0;
+	while (str[i] != '\0')
 	{
-		len++;
-	}
-	for (i = 0; i < len; i++)
-    	{
-		dest[i] = src[i];
+		i++;
 	}
 
-	dest[i] = '\0';
-	return (dest);
-  
+	last = (i + 1) / 2;
+
+	for (i = last; str[i]; i++)
+	{
+		_putchar (str[i]);
+
+	}
+	_putchar ('\n');
 }
