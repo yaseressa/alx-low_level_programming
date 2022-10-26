e2eeed3d1a251ecd1f44fa4b8d70be0534780b61
yaseressa <yaseressa222@gmail.com>
2022-10-26 22:10:25 +0300

    dj

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,7 +8,6 @@
  */
 
 char *_strncpy(char *dest, char *src, int n)
-
 {
 int i;
 
@@ -18,13 +17,10 @@ while (i < n && src[i] != '\0')
 dest[i] = src[i];
 i++;
 }
-
 while (i < n)
 {
 dest[i] = '\0';
 i++;
 }
-
 return (dest);
-
 }
